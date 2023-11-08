1a0deefd48a1a19dff3fb46bac9485b06b1b8944
beyzanurrr <byzanurdeniz@gmail.com>
2023-11-08 21:50:32 +0300

    tried to create member in, not done

--- AVLTree.java
+++ AVLTree.java
@@ -1,173 +1,170 @@
-/*
-@Author: Beyza Nur Deniz
-@Date: 4.11.2023
-@Student ID: 2021400285
- */
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+//TODO: fix insert, printMemberIn to print the bosses in the correct order
+//TODO: look at other operations: MEMBER_OUT, INTEL_TARGET, INTEL_DIVIDE, INTEL_RANK
 
 
-public class AVLTree<AnyType extends Comparable<? super AnyType>> {
-    private Node<AnyType> root;
 
-    /*
-    @GMS is the data of the node
-    @leftChild is the left child of the node
-    @rightChild is the right child of the node
-     */
-    private static class Node<AnyType> {
-        AnyType GMS;
-        Node<AnyType> leftChild;
-        Node<AnyType> rightChild;
+public class AVLTree<K extends String, V extends Double> {
+    private Node<K, V> root;
+    private BufferedWriter writer;
 
-        Node(AnyType GMS) {
-            this(GMS, null, null);
+
+    private void printMemberIn(Node<K, V> node, String name, BufferedWriter writer) {
+        try {
+            writer.write(node.name + " welcomed " + name +  "\n");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
         }
+    }
 
-        Node(AnyType GMS, Node<AnyType> lc, Node<AnyType> rc) {
+    public AVLTree(V GMS, K name, BufferedWriter writer) {
+        this.writer = writer;
+        this.root = null;
+        insert(name, GMS);
+    }
+
+
+    public AVLTree(BufferedWriter writer) {
+        this.root = null;
+        this.writer = writer;
+    }
+
+    private static class Node<K, V> {
+        K name;
+        V GMS;
+        Node<K, V> leftChild;
+        Node<K, V> rightChild;
+        int height;
+        Node<K, V> boss;
+        int rank;
+
+        Node(K name, V GMS) {
+            this.name = name;
             this.GMS = GMS;
-            this.leftChild = lc;
-            this.rightChild = rc;
+            this.leftChild = null;
+            this.rightChild = null;
+            this.height = 1;
+            this.boss = null;
+            this.rank = 1;
         }
     }
-
-    AVLTree() {
-        this.root = null;
+    public void insert(K name, V GMS) {
+        this.root = insert(name, GMS, this.root);
+        find(name, this.root, true);
     }
 
-    /*
-    clear method is used to clear the tree
-     */
-    public void clear() {
-        this.root = null;
+    private Node<K, V> insert(K name, V GMS, Node<K, V> node) {
+        if (node == null) {
+            return new Node<>(name, GMS);
+        }
+        int compareResult = name.compareTo(node.name);
+        if (compareResult < 0) {
+            node.leftChild = insert(name, GMS, node.leftChild);
+        } else if (compareResult > 0) {
+            node.rightChild = insert(name, GMS, node.rightChild);
+        }
+        return balance(node);
     }
 
-    /*
-    isEmpty method is used to check if the tree is empty or not
-    @return true if the tree is empty
-     */
+    private Node<K, V> balance(Node<K, V> node) {
+        if (node == null) {
+            return node;
+        }
+        int balance = height(node.leftChild) - height(node.rightChild);
+        if (balance > 1) {
+            if (height(node.leftChild.leftChild) >= height(node.leftChild.rightChild)) {
+                node = rotateWithLeftChild(node);
+            } else {
+                node = doubleWithLeftChild(node);
+            }
+        } else if (balance < -1) {
+            if (height(node.rightChild.rightChild) >= height(node.rightChild.leftChild)) {
+                node = rotateWithRightChild(node);
+            } else {
+                node = doubleWithRightChild(node);
+            }
+        }
+        return node;
+    }
+
+    private Node<K, V> rotateWithLeftChild(Node<K, V> k2) {
+        Node<K, V> k1 = k2.leftChild;
+        k2.leftChild = k1.rightChild;
+        k1.rightChild = k2;
+        k2.height = Math.max(height(k2.leftChild), height(k2.rightChild)) + 1;
+        k1.height = Math.max(height(k1.leftChild), k2.height) + 1;
+        return k1;
+    }
+
+    private Node<K, V> rotateWithRightChild(Node<K, V> k1) {
+        Node<K, V> k2 = k1.rightChild;
+        k1.rightChild = k2.leftChild;
+        k2.leftChild = k1;
+        k1.height = Math.max(height(k1.leftChild), height(k1.rightChild)) + 1;
+        k2.height = Math.max(height(k2.rightChild), k1.height) + 1;
+        return k2;
+    }
+
+    private Node<K, V> doubleWithLeftChild(Node<K, V> k3) {
+        k3.leftChild = rotateWithRightChild(k3.leftChild);
+        return rotateWithLeftChild(k3);
+    }
+
+    private Node<K, V> doubleWithRightChild(Node<K, V> k1) {
+        k1.rightChild = rotateWithLeftChild(k1.rightChild);
+        return rotateWithRightChild(k1);
+    }
+
     public boolean isEmpty() {
         return this.root == null;
     }
 
-    private int height() {
+    public int height() {
         return height(this.root);
     }
 
-    /*
-    height method is used to find the height of the tree
-    @param node is the node that we want to find the height of
-    @return -1 if the node is null
-     */
-    private int height(Node<AnyType> node) {
-        if (node == null) {
-            return -1;
+    private int height(Node<K, V> node) {
+        return (node != null) ? node.height : 0;
+    }
+
+    public int rank(K name) {
+        Node<K, V> node = find(name, this.root, false);
+        if (node != null) {
+            return rank(node);
+        } else {
+            return -1; // Node not found
         }
-        return 1 + Math.max(height(node.leftChild), height(node.rightChild));
     }
 
-    /*
-    rank method is used to find the rank defined in the homework
-    @param node is the node that we want to find the rank of
-     */
-    private int rank(Node<AnyType> node) {
-        return height(root) - height(node);
-    }
-
-    public int rank(AnyType GMS) {
-        return rank(find(GMS));
-    }
-
-    /*
-    find method is used to find the node that has the given data
-    @param GMS is the data that we want to find
-     */
-    public Node<AnyType> find(AnyType GMS) {
-        return find(GMS, this.root);
-    }
-
-    /*
-    find method is used to find the node that has the given data
-    @param GMS is the data that we want to find
-    @param node is the node that we want to find the data in
-
-    @return null if the node is null
-    @return node if the node has the given data
-    @return find(GMS, node.leftChild) if the node is smaller than the given data
-    @return find(GMS, node.rightChild) if the node is bigger than the given data
-     */
-    private Node<AnyType> find(AnyType GMS, Node<AnyType> node) {
+    private Node<K, V> find(K name, Node<K, V> node, boolean print) {
         if (node == null) {
             return null;
         }
-        if (GMS.compareTo(node.GMS) < 0) {
-            return find(GMS, node.leftChild);
-        } else if (GMS.compareTo(node.GMS) > 0) {
-            return find(GMS, node.rightChild);
+        int compareResult = name.compareTo(node.name);
+        if (compareResult < 0) {
+            if(print){
+                printMemberIn(node, name, this.writer);
+            }
+            return find(name, node.leftChild, print);
+        } else if (compareResult > 0) {
+            if(print){
+                printMemberIn(node, name, this.writer);
+            }
+            return find(name, node.rightChild,print);
         } else {
             return node;
         }
     }
 
-    /*
-    isBalanced method is used to check if the tree is balanced or not
-    @return true if the tree is balanced
-    */
-    public boolean isBalanced() {
-        return isBalanced(this.root);
-    }
-    private boolean isBalanced(Node<AnyType> node) {
-        return Math.abs(height(node.leftChild) - height(node.rightChild)) <= 1;
+    private int rank(Node<K, V> node) {
+        return height(this.root) - height(node);
     }
 
-    /*
-    findMin method is used to find the node that has the minimum data
-    @return null if the tree is empty
-    @return findMin(node.leftChild) if the node has a left child
-    @return node if the node does not have a left child
-     */
-
-    public AnyType findMin() {
-        return findMin(this.root).GMS;
+    public void clear() {
+        this.root = null;
     }
-    private Node<AnyType> findMin(Node<AnyType> node) {
-        if (node == null) {
-            return null;
-        }
-        if (node.leftChild != null) {
-            return findMin(node.leftChild);
-        }
-        return node;
-    }
-
-    /*
-    findMax method is used to find the node that has the maximum data
-    @return null if the tree is empty
-    @return findMax(node.rightChild) if the node has a right child
-    @return node if the node does not have a right child
-     */
-
-    public AnyType findMax() {
-        return findMax(this.root).GMS;
-    }
-    private Node<AnyType> findMax(Node<AnyType> node) {
-        if (node == null) {
-            return null;
-        }
-        if (node.rightChild != null) {
-            return findMax(node.rightChild);
-        }
-        return node;
-    }
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//TODO: create other operations in while loop
+//TODO: add necessary comments and documentation
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: java Main <input_file> <output_file>");
+            return;
+        }
 
+        String inputFilePath = args[0];
+        String outputFilePath = args[1];
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+
+
+            String line;
+            line = reader.readLine();
+
+            String[] parts = line.split(" ");
+            double GMS = Double.parseDouble(parts[1]);
+            String name = parts[0];
+            AVLTree<String, Double> tree = new AVLTree<>(GMS, name, writer);
+
+            while ((line = reader.readLine()) != null) {
+                // Process the content as needed, for example, you can simply write it to the output file
+                parts = line.split(" ");
+                    String command = parts[0];
+                    if(command.equals("MEMBER_IN")){
+                        name = parts[1];
+                        GMS = Double.parseDouble(parts[2]);
+                        tree.insert(name, GMS);
+
+                    }
+
+            }
+
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
     }
 }
